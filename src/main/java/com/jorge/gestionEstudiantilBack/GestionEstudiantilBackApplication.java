89b971ba91f52c08d293755f119d26c54c529438
jorge <jbalmoriarenas@gmail.com>
2024-10-02 11:58:03 +0200

    Primer commit

---
+++ src/main/java/com/jorge/gestionEstudiantilBack/GestionEstudiantilBackApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.gestionEstudiantilBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEstudiantilBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEstudiantilBackApplication.class, args);
+	}
+
+}
